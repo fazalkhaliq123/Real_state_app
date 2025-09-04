0cd30680d173cbb8c9ad555643cb9f87f54fe188
Fazal Khaliq <fazalkhaliq236@gmail.com>
2025-09-04 03:57:12 -0700

    initial commit

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -3,12 +3,13 @@ import axios from "axios";
 export const baseUrl = 'https://bayut.p.rapidapi.com';
 
 export const fetchApi = async (url) => {
-  const { data } = await axios.get((url), {
+  const { data } = await axios.get(url, {
     headers: {
       'x-rapidapi-host': 'bayut.p.rapidapi.com',
-      'x-rapidapi-key': process.env.NEXT_PUBLIC_RAPID_API_KEY ,
+      'x-rapidapi-key': '1538f9f8d9msh1b6e85fb3ccec9cp148117jsn0569c0e8477e', 
     },
   });
-    
+
   return data;
-}
+};
+
